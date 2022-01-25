78f838ebaf68d49ec79b2955f693f6f7d6510d8d
KarloFab <fabijanickarlo1@gmail.com>
2022-01-25 19:50:25 +0100

    Initial Commit

---
+++ ListViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListViewer
+//
+//  Created by Karlo Fabijanić on 25.01.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
