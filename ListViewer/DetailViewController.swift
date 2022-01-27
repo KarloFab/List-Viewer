cdccf76a2f28c958082005e5bc20d4fb376e3de2
KarloFab <fabijanickarlo1@gmail.com>
2022-01-27 21:55:52 +0100

    Added new DetailViewController

---
+++ ListViewer/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  ListViewer
+//
+//  Created by Karlo Fabijanić on 27.01.2022..
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
